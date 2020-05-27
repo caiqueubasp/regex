e891c5b4f47665e8e7e0fc9172ff22187f8c12db
caiqueubasp <caiqueubasp@gmail.com>
2020-05-26 22:56:15 -0300

    Section 3: Brackets and range of characters - advanced patterns

--- regex-exercises/regex1.js
+++ regex-exercises/regex1.js
@@ -43,3 +43,36 @@ console.log(stringD.search(rexD) + "\n\n");
 // Replace method
 let newWord = "Typescript";
 document.write(stringD.replace(rexD, newWord));
+
+// Advanced regukar expressions/patterns using brackets & ranges of characteres
+
+let stringE = "Hello there! cattson cetmon, cotss";
+let rexE = /c[aeiou]t/gi;
+document.write(stringE.match(rexE) + "\n\n");
+
+//Number ranges with brackets
+let stringF = "123 145 143 167 153 173 175 857 5640";
+let rexF = /1[0-9]3/g;
+document.write(stringF.match(rexF) + "\n\n");
+
+//Alphabet ranges within brackets
+let stringG = "Cat caught the rat on the mat and gave it a pat.";
+let rexG = /[a-z]at/gi; //ends with an at
+document.write(stringG.match(rexG) + "\n\n");
+
+// Using pipe characteres to specify options
+
+let stringH = "Cat caught the rat on the mat and gave it a pat.";
+let rexH = /(cat|rat|mat)/gi; //ends with an at
+document.write(stringH.match(rexH) + "\n\n");
+
+//Specifying ranges/characters we don't want in the string - ^char/pattern within your square brackets
+let stringI = "Cat caught the rat on the mat and gave it a pat.";
+let rexI = /[^aeiou .]/gi; //ends with an at
+document.write(stringI.match(rexI) + "\n\n");
+
+//Multiple of the same thing - alphabets, digits or both - 3 caracter
+//substring/words - 2 number $ 1 alphabet (small)
+let stringJ = "cat mat 12t 3g 45a cA5 64A a8D";
+let rexJ = /[0-9a-z][0-9][a-z]/gi; //ends with an at
+document.write(stringJ.match(rexJ) + "\n\n");
